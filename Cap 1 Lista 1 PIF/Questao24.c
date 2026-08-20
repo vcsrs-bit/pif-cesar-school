ab0b2d59e72cae76938447ff9e83fa877e826fb6
vcsrs-bit <vcsrs@cesar.school>
2026-08-19 21:18:35 -0300

    aaaaaaaa

---
+++ Cap 1 Lista 1 PIF/Questao24.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  printf("%-15s %5s\n", "Alunos", "Nota\n");
+  printf("=================================\n");
+  printf("%-15s %6.2f\n", "Aline", 9.0);
+  printf("%-15s %6s\n", "Mário", "DEZ");
+  printf("%-15s %6.2f\n", "Sérgio", 7.0);
+  printf("%-15s %6.2f\n", "Shirley", 4.5);
+  return 0;
+  
+}
